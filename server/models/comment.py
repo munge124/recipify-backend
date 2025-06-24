2a485fca95f70b34154cafe1701f4409df6b1631
alv1nol9 <alvin.kimani@student.moringaschool.com>
2025-06-24 15:18:35 +0300

    Update comment.py

--- server/models/comment.py
+++ server/models/comment.py
@@ -8,3 +8,11 @@ class Comment(db.Model):
     text = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "user": self.author.username,  # from relationship
+            "recipe_id": self.recipe_id,
+        }
